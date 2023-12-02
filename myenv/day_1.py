86a47496e0b35129c80aed8bd2e7feae68b809c6
Parnia Parsa <parnia.parsa@devies.se>
2023-12-03 00:10:51 +0100

    Co-authored-by: Rasmus <Rasfalga@users.noreply.github.com>

--- myenv/day_1.py
+++ myenv/day_1.py
@@ -1,1 +1,18 @@
-print("i lorb my babe")
+import re
+with open('/Users/parniaparsa/Documents/aoc2023/myenv/input_day_1.txt', "r") as text_file:
+    lines = text_file.read().splitlines()
+    text_file.close()
+
+values = []
+
+for line in lines:
+    digits = ''.join(filter(str.isdigit, line))
+    values.append(int(str(digits[0] + digits[len(digits)-1])))
+    
+summa = 0
+for i in range(0, len(values)):    
+   summa = summa + values[i]
+print(summa)
+      
+  
+
